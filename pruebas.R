fcceead9186ac2801f2fc19acedb127bdcef5d17
Gonzalo Barrera Borla <gonzalo.barrera@gmail.com>
2019-11-26 02:31:16 -0300

    Agrega summaries ad-hoc con infraestructura de caret

--- pruebas.R
+++ pruebas.R
@@ -1,22 +1,18 @@
 library(broom)
 library(tidyverse)
 library(caret)
-library(RobStatTM)
+# Si tiene problemas instalando glmnet en R < 3.6, necesita una version 2.x de glmnet
+# glmnetURL <- "https://cran.r-project.org/src/contrib/Archive/glmnet/glmnet_2.0-18.tar.gz"
+# install.packages(glmnetURL, repos=NULL, type="source")
+library(glmnet)
+
 datos <- read_table(
   "Concurso_Estima.txt",
   col_names = c('ciclo', 'minmem', 'maxmem', 'cache', 'mincan', 'maxcan', 'perfo')
-)
-
-datos <- mutate(
-  datos,
+) %>% mutate(
   freq = 1/ciclo,
   promcan = (mincan + maxcan)/2,
-  prommem = (minmem + maxmem)/2,
-  cach = cache/10,
-  chavg = ceiling((mincan + maxcan)/2),
-  speed = 1/ciclo,
-  chcap = chavg * speed * 10,
-  mavg = (minmem + maxmem)/2 * 10 - 3
+  prommem = (minmem + maxmem)/2
 )
 
 comparar <- function(modelos, datos, control) {
@@ -31,98 +27,137 @@ comparar <- function(modelos, datos, control) {
   return(resumen)
 }
 
-postResamplePodado <- function (pred, obs, alfa) {
-  isNA <- is.na(pred)
-  pred <- pred[!isNA]
-  obs <- obs[!isNA]
-  if (!is.factor(obs) && is.numeric(obs)) {
-    if (length(obs) + length(pred) == 0) {
-      out <- rep(NA, 3)
-    }
-    else {
-      if (length(unique(pred)) < 2 || length(unique(obs)) < 
-          2) {
-        resamplCor <- NA
+postResampleAdHoc <- function(alfa = 1, inversa = identity) {
+  post_resample <- function(pred, obs){
+    isNA <- is.na(pred)
+    pred <- inversa(pred[!isNA])
+    obs <- inversa(obs[!isNA])
+    if (!is.factor(obs) && is.numeric(obs)) {
+      if (length(obs) + length(pred) == 0) {
+        out <- rep(NA, 3)
       }
       else {
-        resamplCor <- try(cor(pred, obs, use = "pairwise.complete.obs"), 
-                          silent = TRUE)
-        if (class(resamplCor) == "try-error") 
+        if (length(unique(pred)) < 2 || length(unique(obs)) < 
+            2) {
           resamplCor <- NA
+        }
+        else {
+          resamplCor <- try(cor(pred, obs, use = "pairwise.complete.obs"), 
+                            silent = TRUE)
+          if (class(resamplCor) == "try-error") 
+            resamplCor <- NA
+        }
+        SqErr <- (pred - obs)^2
+        poda <- SqErr <= quantile(SqErr, alfa)
+        msePodado <- mean(SqErr[poda])
+        rSqPodado <- cor(pred[poda], obs[poda], use = "pairwise.complete.obs")
+        mse <- mean(SqErr)
+        mae <- mean(abs(pred - obs))
+        out <- c(sqrt(msePodado), rSqPodado^2, sqrt(mse), resamplCor^2, mae)
       }
-      SE <- (pred - obs)^2
-      SE_ <- SE[SE <= quantile(SE, alfa)]
-      mse <- mean(SE_)
-      mae <- mean(abs(pred - obs))
-      out <- c(sqrt(mse), resamplCor^2, mae)
-    }
-    names(out) <- c("RMSE", "Rsquared", "MAE")
-  }
-  else {
-    if (length(obs) + length(pred) == 0) {
-      out <- rep(NA, 2)
+      names(out) <- c("aRMSE", "aRsquared", "RMSE", "Rsquared", "MAE")
     }
     else {
-      pred <- factor(pred, levels = levels(obs))
-      requireNamespaceQuietStop("e1071")
-      out <- unlist(e1071::classAgreement(table(obs, pred)))[c("diag", 
-                                                               "kappa")]
+      if (length(obs) + length(pred) == 0) {
+        out <- rep(NA, 2)
+      }
+      else {
+        pred <- factor(pred, levels = levels(obs))
+        requireNamespaceQuietStop("e1071")
+        out <- unlist(e1071::classAgreement(table(obs, pred)))[c("diag", 
+                                                                 "kappa")]
+      }
+      names(out) <- c("Accuracy", "Kappa")
     }
-    names(out) <- c("Accuracy", "Kappa")
+    if (any(is.nan(out))) 
+      out[is.nan(out)] <- NA
+    out
   }
-  if (any(is.nan(out))) 
-    out[is.nan(out)] <- NA
-  out
+  return(post_resample)
 }
 
-defaultSummaryPodado <- function (data, lev = NULL, model = NULL) {
-  if (is.character(data$obs)) 
-    data$obs <- factor(data$obs, levels = lev)
-  postResamplePodado(data[, "pred"], data[, "obs"], 0.85)
+adHocSummary <- function(alfa = 1, inversa = identity){
+  post_resample <- postResampleAdHoc(alfa, inversa)
+  function(data, ...) {
+    post_resample(data[, "pred"], data[, "obs"])
+  }
 }
 
-modelos <- list(
-  perfo ~ freq + minmem + maxmem + cache + mincan + maxcan,
-  
-  perfo ~ (freq + minmem + maxmem + cache + mincan + maxcan)^2,
-  perfo ~ (freq + minmem + maxmem + cache + mincan + maxcan)^3,
-  
-  perfo ~ cach + chcap + mavg,
-  perfo ~ cach + chcap + log(mavg),
-  perfo ~ (cach + chcap + log(mavg))^2,
-  perfo ~ cach + chcap + mavg,
-  
-  perfo ~ (cach + chcap + mavg)^2,
-  perfo ~ (cach + chcap + mavg)^2 + cach*chcap*mavg,
-  perfo ~ (cach + chcap + mavg)^3,
-  perfo ~ polym(cach, chcap, mavg, degree = 2),
-  
+modelosIdenticos <- list(
+  perfo ~ freq + prommem + cache + promcan,
+  perfo ~ (freq + prommem + cache + promcan)^2,
+  perfo ~ (freq + prommem + cache + promcan)^3,
   perfo ~ polym(freq, minmem, maxmem, cache, mincan, maxcan, degree = 2),
-  perfo ~ polym(freq, prommem, cache, promcan, degree = 2)
-  # sqrt(perfo) ~ ciclo + minmem + maxmem + cache + mincan + maxcan,
-  # sqrt(perfo) ~ (ciclo + minmem + maxmem + cache + mincan + maxcan)^2,
-  # sqrt(perfo) ~ cach + chavg + speed + chcap + mavg,
-  # sqrt(perfo) ~ (cach + chavg + speed + chcap + mavg)^2
+  perfo ~ polym(freq, prommem, cache, promcan, degree = 2),
+  perfo ~ polym(freq, prommem, cache, promcan, degree = 3),
+  perfo ~ polym(freq, prommem, cache, promcan, degree = 4)
 )
 
-trCont <- trainControl(
-  summaryFunction = defaultSummaryPodado,
+modelosRadicales <- list(
+  sqrt(perfo) ~ freq + prommem + cache + promcan,
+  sqrt(perfo) ~ (freq + prommem + cache + promcan)^2,
+  sqrt(perfo) ~ (freq + prommem + cache + promcan)^3,
+  sqrt(perfo) ~ polym(freq, minmem, maxmem, cache, mincan, maxcan, degree = 2),
+  sqrt(perfo) ~ polym(freq, prommem, cache, promcan, degree = 2),
+  sqrt(perfo) ~ polym(freq, prommem, cache, promcan, degree = 3),
+  sqrt(perfo) ~ polym(freq, prommem, cache, promcan, degree = 4)
+)
+
+modelosLogaritmicos <- list(
+  log(perfo) ~ freq + prommem + cache + promcan,
+  log(perfo) ~ (freq + prommem + cache + promcan)^2,
+  log(perfo) ~ (freq + prommem + cache + promcan)^3,
+  log(perfo) ~ polym(freq, minmem, maxmem, cache, mincan, maxcan, degree = 2),
+  log(perfo) ~ polym(freq, prommem, cache, promcan, degree = 2),
+  log(perfo) ~ polym(freq, prommem, cache, promcan, degree = 3),
+  log(perfo) ~ polym(freq, prommem, cache, promcan, degree = 4)
+)
+
+alfa <- 0.85
+summaryId <- adHocSummary(alfa, identity) # Id(entidad)
+summaryRad <- adHocSummary(alfa, function(x) x^2) # Rad(ical)
+summaryLog <- adHocSummary(alfa, exp)
+
+trContId <- trainControl(
+  summaryFunction = summaryId,
+  method = "LGOCV", number = 100, p = 0.75
+)
+trContRad <- trainControl(
+  summaryFunction = summaryRad,
+  method = "LGOCV", number = 100, p = 0.75
+)
+trContLog <- trainControl(
+  summaryFunction = summaryLog,
   method = "LGOCV", number = 100, p = 0.75
 )
 
-comparar(modelos, datos, trCont)
+idres <- comparar(modelosIdenticos, datos, trContId)
+radres <- comparar(modelosRadicales, datos, trContRad)
+logres <- comparar(modelosLogaritmicos, datos, trContLog)
+res <- bind_rows(idres, radres, logres)
 
-slm(perfo ~ polym(freq, prommem, cache, promcan, degree = 2),
-   datos) %>% tidy()
-lm(perfo ~ polym(freq, minmem, maxmem, cache, mincan, maxcan, degree = 2),
-   datos) %>% summary()
-
+unmodelo <- modelosRadicales[[5]]
 glmnetGrid <- expand.grid(
-  alpha = seq(0, 1, 0.25),
-  lambda = 10^seq(-1, 3, 0.1)
+  alpha = c(1),
+  lambda = 10^seq(-1.5, 1, 0.05)
 )
-train(perfo ~ polym(freq, prommem, cache, promcan, degree = 2), datos,
-      trControl = trCont, method = "glmnet", tuneGrid = glmnetGrid)
 
-train(perfo ~ polym(freq, prommem, cache, promcan, degree = 2), datos,
-      trControl = trCont, method = "pls", tuneGrid = expand.grid(ncomp = 1:10))
+lassores <- train(unmodelo,
+      datos,
+      trControl = trContRad, metric = "aRMSE", maximize = FALSE,
+      method = "glmnet", tuneGrid = glmnetGrid
+)
+plsres <- train(unmodelo,
+      datos,
+      trControl = trContRad, metric = "aRMSE", maximize = FALSE,
+      method = "pls", tuneGrid = expand.grid(ncomp = 1:20)
+)
+
+
+modelo <- lm(disp ~ cyl, mtcars)
+predecir <- function(newdata){
+  predict(modelo, newdata)
+}
+
+
+save("modelo", "predecir", file = "modelo.RData")
